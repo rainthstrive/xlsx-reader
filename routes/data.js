f9a58e9e949cd36aee3c75b06ffc61aaea86ef5c
Arturo Garcia <rainthstrive@gmail.com>
2023-05-09 15:52:05 -0600

    Functional POC

--- routes/data.js
+++ routes/data.js
@@ -1,27 +1,99 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const multer = require('multer');
-const upload = multer({ dest: 'uploads/'});
-const xlsx = require('xlsx');
+// Multer para subida de archivos
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+// XLSX para manejo de archivos Excel
+const xlsx = require("xlsx");
+// Mongoose para manejo de MongoDB
+const mongoose = require("mongoose");
+// Conexión abierta de MongoDB para manejar todas las peticiones
+mongoose
+    .connect(
+      "mongodb+srv://admin2023:admin2023@rainth-test-cluster.6abyclm.mongodb.net/?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    )
+    .then(() => {
+      console.log("Conexión exitosa a MongoDB");
+    })
+    .catch((error) => {
+      console.error("Error al conectar a MongoDB: ", error);
+    });
+// Definición del schema para MongoDB
+const workbookSchema = new mongoose.Schema({
+  estado: {
+    type: String,
+    enum: ["pending", "processing", "done"],
+    default: "pending",
+  },
+  datos: [
+    {
+      Nombre: String,
+      Edad: Number,
+      Nums: [Number],
+    },
+  ],
+  errores: [
+    {
+      info: String,
+    },
+  ],
+});
+// Modelo del schema
+const workbookModel = mongoose.model("WbRequest", workbookSchema);
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post("/upload", upload.single("file"), async function (req, res, next) {
+  const filePath = xlsx.readFile(req.file.path);
+
+  try {
+    // Crear el documento del pedido con el estado "pendiente"
+    const nuevoObjeto = await workbookModel.create({});
+    // Devolver solo el ID del documento como respuesta inicial
+    res.status(201).json({ id: nuevoObjeto._id });
+    processXLSXFile(filePath, nuevoObjeto._id);
+  } catch(error) {
+    // Manejo interno de errores
+    console.error('Error al subir el objeto a MongoDB: ', error);
+    res.status(500).json({error: 'Error de base de datos'});
+  }
+  
 });
 
-router.post('/upload', upload.single('file'), function(req, res, next) {
-  const workbook = xlsx.readFile(req.file.path);
-  const sheet_name_list = workbook.SheetNames;
-  const data = xlsx.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
-  data.forEach(row => {
-    row.Edad = Number(row.Edad);
-  });
-  data.forEach(row => {
-    row.Nums = row.Nums.split(',').map(value => Number(value.trim()));
-  });
-  res.send(data);
-})
+async function processXLSXFile(filePath, objetoId){
+  try {
+    const sheet_name_list = filePath.SheetNames;
+    const data = xlsx.utils.sheet_to_json(filePath.Sheets[sheet_name_list[0]]);
+    data.forEach((row) => {
+      row.Edad = Number(row.Edad);
+    });
+    data.forEach((row) => {
+      row.Nums = row.Nums.split(",").map((value) => Number(value.trim()));
+    });
+    console.log('calling');
+    const result = await resolveAfter10Seconds();
+    console.log(result);
+    await workbookModel.findByIdAndUpdate(objetoId, { 
+      estado: 'done', 
+      datos: data
+    });
+    console.log(data);
+  } catch(error) {
+    // Manejo interno de errores
+    console.error('Error al procesar el archivo XLXS: ', error);
+    await workbookModel.findByIdAndUpdate(objetoId, { estado: 'done', errores: error });
+  }
+}
 
+function resolveAfter10Seconds() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve('resolved');
+    }, 10000);
+  });
+}
 
 module.exports = router;
